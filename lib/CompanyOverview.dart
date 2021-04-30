0eddc687a33e8441b3d8a533930e8766f981a87f
Niklas Wortmann <nwortmann@icloud.com>
2021-04-30 20:47:35 +0200

    add data fetching in overview

--- lib/CompanyOverview.dart
+++ lib/CompanyOverview.dart
@@ -1,4 +1,6 @@
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class CompanyOverview extends StatefulWidget {
   final String symbol;
@@ -10,12 +12,64 @@ class CompanyOverview extends StatefulWidget {
 }
 
 class _CompanyOverviewState extends State<CompanyOverview> {
+  late Future<CompanyProfile> futureProfile;
+
+  // TODO: Implement error handling
+  Future<CompanyProfile> fetchCompanyProfile() async {
+    var dio = Dio();
+    final res = await dio.get(
+      'https://financialmodelingprep.com/api/v3/profile/${widget.symbol}?apikey=${env['FMP_API_KEY']}',
+    );
+
+    if (res.statusCode == 200) {
+      final data = res.data as List;
+      return new CompanyProfile.fromJson(data, 0);
+    }
+    return new CompanyProfile(
+        companyName: '...', imageUrl: '...', description: '...', price: 0);
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    futureProfile = fetchCompanyProfile();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        child: Text(widget.symbol),
+      body: FutureBuilder<CompanyProfile>(
+        future: futureProfile,
+        builder: (context, snapshot) {
+          final companyOverview = snapshot.data!;
+          return snapshot.hasData
+              ? Card(
+                  child: Text(companyOverview.companyName),
+                )
+              : CircularProgressIndicator();
+        },
       ),
     );
   }
+}
+
+class CompanyProfile {
+  final String companyName;
+  final String imageUrl;
+  final String description;
+  final double price;
+
+  CompanyProfile(
+      {required this.companyName,
+      required this.imageUrl,
+      required this.description,
+      required this.price});
+
+  factory CompanyProfile.fromJson(List<dynamic> json, int index) =>
+      CompanyProfile(
+        companyName: json[index]['companyName'],
+        imageUrl: json[index]['image'],
+        description: json[index]['description'],
+        price: json[index]['price'],
+      );
 }
